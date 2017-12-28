a4a77953101c6b8e4275d9b4d9b416c9242a9ec5
bob-nx <me@nagexiucai.com>
2017-12-28 14:25:02 +0800

    调试通dingtalk-contacts-pickup.py。

--- core/__init__.py
+++ core/__init__.py
@@ -3,12 +3,10 @@
 
 import tensorflow as tf
 import numpy as np
+import cv2 as cv
 from pprint import pprint
 
 # verify installation
-# hello = tf.constant('Hello, TensorFlow!')
-# sess = tf.Session()
-# print(sess.run(hello))
-
-z = tf.zeros([3,4,5])
-pprint(z)
+# hi = tf.constant('Hi,TF!')
+# session = tf.Session()
+# print(session.run(hi))
